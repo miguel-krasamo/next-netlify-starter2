1523970a6f46294ece94ed5e5e22e9e7ba05fdb5
miguel-krasamo <77803491+miguel-krasamo@users.noreply.github.com>
2023-12-06 13:45:56 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,16 +6,9 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>You Need to Change your Password</title>
+        
       </Head>
-
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
 
       <Footer />
     </div>
